4321fc7433562a8c3c5002c65f6dd1b79717372f
Damián Farina <damianfarina@gmail.com>
2021-12-22 13:36:55 -0300

    First commit of https://github.com/timlrx/tailwind-nextjs-starter-blog

---
+++ data/headerNavLinks.js
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Proyectos' },
+  { href: '/about', title: 'Acerca de' },
+]
+
+export default headerNavLinks
