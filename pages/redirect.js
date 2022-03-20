6023a0eb5a0b599398245f9253b2fcbbbb69945b
Damián Farina <damianfarina@gmail.com>
2022-03-20 20:21:55 -0300

    QR redirecciona al home

--- pages/redirect.js
+++ pages/redirect.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 export default function Redirect() {
   useEffect(() => {
-    window.location = '/acerca-de'
+    window.location = '/'
   }, [])
 
   return null
